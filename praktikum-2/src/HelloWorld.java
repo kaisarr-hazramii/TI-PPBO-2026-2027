1e83fa9c864264372db48311bdcb5e420832bd7d
kaisar hazrami <kaisarr0085@gmail.com>
2026-09-07 11:33:19 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("kaisar hazrami!");
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
 
 
     }

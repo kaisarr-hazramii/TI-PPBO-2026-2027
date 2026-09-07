b978825fa01eb0f0c80746168a9410dfdfec4c81
kaisar hazrami <kaisarr0085@gmail.com>
2026-09-07 12:17:29 +0700

    Casting(Konversi Tipe Data)

--- praktikum-2/src/VariabelDemo.java
+++ praktikum-2/src/VariabelDemo.java
@@ -1,6 +1,6 @@
 public class VariabelDemo {
     public static void main(String[] args) {
-        String nama = "Siti";
+        String nama = "rangga";
         int umur = 20;
         double tinggi = 165.5;
         char golDarah ='0';
@@ -11,5 +11,14 @@ public class VariabelDemo {
         System.out.println("Tinggi: " + tinggi);
         System.out.println("Golongan Darah: " + golDarah);
         System.out.println("Aktif:" + mahasiswaAktif);
+
+        int nilaiBulat = 9;
+        double nilaiDoble = nilaiBulat; //widening otomatis
+        System.out.println("Widening: " + nilaiDoble);
+
+        double pecahan = 9.8;
+        int hasilCasting = (int) pecahan; // narrowing eksplisit
+        System.out.println("Narrowing:" + hasilCasting);
+
     }
 }
